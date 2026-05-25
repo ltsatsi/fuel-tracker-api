b48041a153ec0440100ea2cd054f072e8d2f2bd6
ltsatsi <ltsatsi18@gmail.com>
2026-05-26 00:25:35 +0200

    Added StorageService

---
+++ Service_Layer/IService/IStorageService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Service_Layer.IService
+{
+    public interface IStorageService
+    {
+        Task<string> GetImageUrl(string path);
+        Task<string> UploadImageAsync(IFormFile file, string folder);
+        Task DeleteImageAsync(string path);
+    } // end interface
+} // end namespace
---
+++ Service_Layer/Service/SupabaseStorageService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Service_Layer.IService;
+using System.Text;
+using Supabase;
+
+namespace Service_Layer.Service
+{
+    public class SupabaseStorageService : IStorageService
+    {
+        private readonly Supabase.Client _supabase;
+        public SupabaseStorageService(Supabase.Client supabase)
+        {
+            _supabase = supabase ?? throw new ArgumentNullException(nameof(supabase));
+        }
+        public async Task<string> GetImageUrl(string path)
+        {
+            return _supabase.Storage.From("images").GetPublicUrl(path);
+        }
+
+        public async Task DeleteImageAsync(string path)
+        {
+            await _supabase.Storage.From("images").Remove(new List<string> { path });
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile file, string folder)
+        {
+            var extension = Path.GetExtension(file.FileName);
+            var fileName = $"{folder}/{Guid.NewGuid()}{extension}";
+
+            using var stream = new MemoryStream();
+
+            await file.CopyToAsync(stream);
+
+            var bytes = stream.ToArray();
+
+            await _supabase.Storage.From("images").Upload(bytes, fileName);
+
+            return fileName;
+        }
+    } // end class
+} // end namespace
